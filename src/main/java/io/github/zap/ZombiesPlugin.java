c4f2a64da9fda59a6e3dc726e95b933689e52d07
John_DND <dnd.initiative.tracker@gmail.com>
2020-10-11 02:37:25 -0700

    Configured the pom even more to properly copy over MythicMobs (it must be included in the plugins folder as a separate jar so Paper can run it).
    Also includes lombok dependency and some initial code in ZombiesPlugin.

--- src/main/java/io/github/zap/ZombiesPlugin.java
+++ src/main/java/io/github/zap/ZombiesPlugin.java
@@ -1,17 +1,37 @@
 package io.github.zap;
 
+import lombok.Getter;
+import org.apache.commons.lang.time.StopWatch;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
 public final class ZombiesPlugin extends JavaPlugin {
+    @Getter
+    private static ZombiesPlugin instance; //singleton pattern for our root plugin class
+
+    @Getter
+    private StopWatch timer;
+
     @Override
-    public void onEnable() {
-        super.onEnable();
+    public void onLoad() { //called before onEnable()
+        instance = this; //we can't give this class a constructor, so use onLoad instead for assigning singleton instance
+        timer = new StopWatch();
     }
 
     @Override
-    public void onDisable() {
-        super.onDisable();
+    public void onEnable() {
+        try {
+            timer.start();
+            super.onEnable();
+
+            //...plugin enabling code here
+
+            timer.stop();
+            super.getLogger().log(Level.INFO, String.format("Done enabling: ~%sms", timer.getTime()));
+        }
+        finally { //ensure profiler gets reset even if we have an exception
+            timer.reset();
+        }
     }
 }
