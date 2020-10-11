6b4de3e0a0138f8bc5c03d843c592129b899950b
John_DND <dnd.initiative.tracker@gmail.com>
2020-10-10 20:57:05 -0700

    Initial commit.

---
+++ src/main/java/io/github/zap/ZombiesPlugin.java
@@ -0,0 +1,15 @@
+package io.github.zap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZombiesPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
