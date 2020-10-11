a398d3c40c95f23147ff51292339f18370bc0200
John_DND <dnd.initiative.tracker@gmail.com>
2020-10-10 22:55:09 -0700

    Configured the pom.xml to produce cleaner output. It also automatically writes the shaded jar to the plugin directory of the test server, which is provided as an argument to the maven command line. This should work for everyone as long as the correct path is supplied!

--- src/main/java/io/github/zap/ZombiesPlugin.java
+++ src/main/java/io/github/zap/ZombiesPlugin.java
@@ -2,6 +2,8 @@ package io.github.zap;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class ZombiesPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
