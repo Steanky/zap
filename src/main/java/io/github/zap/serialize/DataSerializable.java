df080d974c08be0ec85d64ce85d8e29b5698c312
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2020-10-22 22:29:00 -0400

    Shortens DataSerializable method lengths

--- src/main/java/io/github/zap/serialize/DataSerializable.java
+++ src/main/java/io/github/zap/serialize/DataSerializable.java
@@ -6,7 +6,6 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,101 +16,28 @@ public abstract class DataSerializable implements ConfigurationSerializable {
     @Override
     public Map<String, Object> serialize() {
         Map<String, Object> serializedData = new HashMap<>();
-        Field[] fields = this.getClass().getDeclaredFields();
 
-        for(Field field : fields) {
-            if(!Modifier.isStatic(field.getModifiers())) { //skip static fields
-                Annotation[] annotations = field.getDeclaredAnnotations();
-
-                for(Annotation annotation : annotations) {
-                    if(annotation.annotationType() == Serialize.class) {
-                        if(!field.isAccessible()) {
-                            field.setAccessible(true); //serialize private members too
-                        }
-
-                        Serialize serializeAnnotation = (Serialize)annotation;
-                        String annotationName = serializeAnnotation.name();
-
-                        try {
-                            serializedData.put(annotationName.equals(StringUtils.EMPTY) ? field.getName() :
-                                    annotationName, field.get(this));
-                        } catch (IllegalAccessException e) {
-                            ZombiesPlugin.getInstance().getLogger().warning(String.format("Exception when attempting " +
-                                    "to serialize field '%s' in object '%s': %s", field.toGenericString(),
-                                    this.toString(), e.getMessage()));
-                        }
-                        break;
-                    }
-                }
+        SerializeUtil.forEachSerializable(this.getClass(), (field, name) -> {
+            try {
+                String serializedName = name.equals(StringUtils.EMPTY) ? field.getName() : name;
+                serializedData.put(serializedName, field.get(this));
+            } catch (IllegalAccessException e) {
+                ZombiesPlugin.getInstance().getLogger().warning(String.format("Exception when attempting " +
+                                "to serialize field '%s' in object '%s': %s", field.toGenericString(),
+                        this.toString(), e.getMessage()));
             }
-        }
+        });
 
         return serializedData;
     }
 
     public static DataSerializable deserialize(Map<String, Object> data) {
-        //get the classname from the type key, which will be the class we want to construct
-        String className = (String)data.get(ConfigurationSerialization.SERIALIZED_TYPE_KEY);
+        // get the classname from the type key, which will be the class we want to construct
+        String className = (String) data.get(ConfigurationSerialization.SERIALIZED_TYPE_KEY);
 
-        if(className != null) {
+        if (className != null) {
             try {
-                //get the class from the classname
-                Class<?> instanceClass = Class.forName(className);
-
-                if(DataSerializable.class.isAssignableFrom(instanceClass)) { //check if our instance class is DataSerializable
-                    Constructor<?> constructor = instanceClass.getDeclaredConstructor(); //get the parameterless constructor
-
-                    if(!constructor.isAccessible()) {
-                        constructor.setAccessible(true); //required parameterless constructor can be private
-                    }
-
-                    Object instanceObject = constructor.newInstance(); //instantiate the object
-                    Field[] fields = instanceClass.getDeclaredFields();
-
-                    for(Field field : fields) {
-                        if(!Modifier.isStatic(field.getModifiers())) { //do not deserialize static members
-                            Annotation[] annotations = field.getDeclaredAnnotations();
-
-                            for(Annotation annotation : annotations) {
-                                if(annotation.annotationType() == Serialize.class) { //only process @Serialize
-                                    if(!field.isAccessible()) {
-                                        field.setAccessible(true); //deserialize private members too
-                                    }
-
-                                    Serialize serializeAnnotation = (Serialize)annotation;
-                                    String annotationName = serializeAnnotation.name();
-                                    String name = annotationName.equals(StringUtils.EMPTY) ? field.getName() : annotationName;
-
-                                    try {
-                                        Object rawValue = data.get(name); //get the serialized data
-                                        Class<?> fieldType = field.getType();
-
-                                        //try to modify the value that we get from ConfigurationSerialization
-                                        if(rawValue instanceof ArrayList && fieldType.isArray()) {
-                                            //workaround for ConfigurationSerialization giving us arraylists when we need arrays
-                                            rawValue = toArrayDeep((ArrayList<?>)rawValue, fieldType);
-
-                                            //TODO: make it so that lists of arrays deserialize into lists of arrays instead of lists of lists
-                                        }
-
-                                        field.set(instanceObject, rawValue);
-                                    } catch (IllegalAccessException | IllegalArgumentException e) {
-                                        ZombiesPlugin.getInstance().getLogger().warning(String.format("Exception " +
-                                                        "when attempting to serialize field '%s' in object '%s': %s",
-                                                field.toGenericString(), data.toString(), e.getMessage()));
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    return (DataSerializable)instanceObject;
-                }
-                else {
-                    ZombiesPlugin.getInstance().getLogger().warning(String.format("'%s' is not an instance of " +
-                            "DataSerializable", className));
-                }
+                return createDeserialized(className, data);
             } catch (ClassNotFoundException ignored) {
                 ZombiesPlugin.getInstance().getLogger().warning(String.format("'%s' cannot be found", className));
             } catch (IllegalAccessException | InstantiationException | NoSuchMethodException |
@@ -128,7 +54,72 @@ public abstract class DataSerializable implements ConfigurationSerializable {
         return null;
     }
 
-    //recursive utility function: deep-converts arraylists into arrays (handles any 'dimension' of arraylist)
+    /**
+     * Creates an actual deserialized object
+     * @param className The name of the class to deserialize
+     * @param data The data to deserialize with
+     * @return The deserialized data
+     * @throws ClassNotFoundException Invalid class name
+     * @throws NoSuchMethodException General deserialization error
+     * @throws IllegalAccessException General deserialization error
+     * @throws InvocationTargetException General deserialization error
+     * @throws InstantiationException General deserialization error
+     */
+    private static DataSerializable createDeserialized(String className, Map<String, Object> data) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // get the class from the classname
+        Class<?> instanceClass = Class.forName(className);
+
+        if(DataSerializable.class.isAssignableFrom(instanceClass)) { //check if our instance class is DataSerializable
+            Constructor<?> constructor = instanceClass.getDeclaredConstructor(); //get the parameterless constructor
+
+            if(!constructor.isAccessible()) {
+                constructor.setAccessible(true); //required parameterless constructor can be private
+            }
+
+            Object instanceObject = constructor.newInstance(); //instantiate the object
+            SerializeUtil.forEachSerializable(instanceClass, (field, name) -> setDeserializedField(instanceObject, data, name, field));
+
+            return (DataSerializable) instanceObject;
+        }
+        else {
+            ZombiesPlugin.getInstance().getLogger().warning(String.format("'%s' is not an instance of " +
+                    "DataSerializable", className));
+        }
+
+        return null;
+    }
+
+    /**
+     * Sets the field of a deserialized object
+     * @param instanceObject The object that will be deserialized
+     * @param data The serialized data to modify the object
+     * @param name The name of the field in the serialized representation
+     * @param field The field to set in the object
+     */
+    private static void setDeserializedField(Object instanceObject, Map<String, Object> data, String name, Field field) {
+        String serializedName = name.equals(StringUtils.EMPTY) ? field.getName() : name;
+
+        try {
+            Object rawValue = data.get(serializedName); // get the serialized data
+            Class<?> fieldType = field.getType();
+
+            // try to modify the value that we get from ConfigurationSerialization
+            if (rawValue instanceof ArrayList && fieldType.isArray()) {
+                // workaround for ConfigurationSerialization giving us arraylists when we need arrays
+                rawValue = toArrayDeep((ArrayList<?>)rawValue, fieldType);
+
+                // TODO: make it so that lists of arrays deserialize into lists of arrays instead of lists of lists
+            }
+
+            field.set(instanceObject, rawValue);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            ZombiesPlugin.getInstance().getLogger().warning(String.format("Exception " +
+                            "when attempting to serialize field '%s' in object '%s': %s",
+                    field.toGenericString(), data.toString(), e.getMessage()));
+        }
+    }
+
+    // recursive utility function: deep-converts arraylists into arrays (handles any 'dimension' of arraylist)
     private static Object toArrayDeep(ArrayList<?> arrayList, Class<?> fieldType) {
         Class<?> fieldComponent = fieldType.getComponentType();
 
