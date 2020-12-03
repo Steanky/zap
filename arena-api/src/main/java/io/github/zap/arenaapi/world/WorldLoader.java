170c907023cc6ae3cb638e873c2d67ba95d28078
John-DND <dnd.initiative.tracker@gmail.com>
2020-12-03 01:15:43 -0800

    Commit before changing branches

--- arena-api/src/main/java/io/github/zap/arenaapi/world/WorldLoader.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/world/WorldLoader.java
@@ -11,25 +11,24 @@ public interface WorldLoader {
     void preload();
 
     /**
-     * Loads the map associated with worldName. This should copy from worlds cached via preloadWorlds, if possible.
+     * Loads the map associated with worldName. This should copy from worlds cached via preload, if possible.
      * Implementations of this function may run fully or partially async.
      * @param worldName The name of the world to load from
-     * @param worldConsumer The consumer that is called when the world is loaded. This should always be run on the main
+     * @param worldConsumer The consumer that is called when the world is loaded. This should always be run by the main
      *                      server thread
      */
     void loadWorld(String worldName, Consumer<World> worldConsumer);
 
     /**
-     * Unloads the world associated with the name. This should remove the world from Bukkit's server list, in addition
-     * to performing any necessary cleanup.
-     * @param world The name of the specified map
+     * Unloads the specified world. Implementations may throw an exception if the world is unloaded already.
+     * @param world The name of the specified world to unload
      */
     void unloadWorld(World world);
 
     /**
-     * Determine if the specified world exists. It may not be loaded.
-     * @param worldName The world to test
-     * @return Whether or not the world exists
+     * Determine if a world with the specified name exists.
+     * @param worldName The world name to test
+     * @return Whether or not a world with the provided name exists
      */
     boolean worldExists(String worldName);
 }
