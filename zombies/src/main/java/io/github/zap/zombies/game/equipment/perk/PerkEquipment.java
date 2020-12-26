ae0813becb92a31877a80faa1031cbf55a02d932
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2020-12-25 20:11:53 -0500

    compiler error

--- zombies/src/main/java/io/github/zap/zombies/game/equipment/perk/PerkEquipment.java
+++ zombies/src/main/java/io/github/zap/zombies/game/equipment/perk/PerkEquipment.java
@@ -1,13 +1,14 @@
 package io.github.zap.zombies.game.equipment.perk;
 
 import io.github.zap.zombies.game.data.equipment.perk.PerkData;
+import io.github.zap.zombies.game.data.level.PerkLevel;
 import io.github.zap.zombies.game.equipment.UpgradeableEquipment;
 import org.bukkit.entity.Player;
 
 /**
  * Represents a perk hotbar equipment
  */
-public class PerkEquipment extends UpgradeableEquipment<PerkData> {
+public class PerkEquipment extends UpgradeableEquipment<PerkData, PerkLevel> {
     public PerkEquipment(Player player, int slotId, PerkData equipmentData) {
         super(player, slotId, equipmentData);
     }
