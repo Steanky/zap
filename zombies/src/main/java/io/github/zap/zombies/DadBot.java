b0a89c134af2d58bab5640a01a6bfa32f69127ac
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2021-01-08 14:58:57 -0500

    improved everything

---
+++ zombies/src/main/java/io/github/zap/zombies/DadBot.java
@@ -0,0 +1,16 @@
+package io.github.zap.zombies;
+
+/**
+ * Needs to be in UT
+ */
+public class DadBot {
+
+    /**
+     * Executes perfection
+     * @param name Someone's name
+     */
+    public String onIAm(String name) {
+        return String.format("Hi %s, I'm Dad Bot!%n", name);
+    }
+
+}
