33ef1e29dcf58e5f2f5596397392e2af5275552d
John-DND <dnd.initiative.tracker@gmail.com>
2021-01-29 01:46:05 -0800

    Partial rewrite.

--- zombies/src/main/java/io/github/zap/zombies/command/mapeditor/form/NewMapForm.java
+++ zombies/src/main/java/io/github/zap/zombies/command/mapeditor/form/NewMapForm.java
@@ -6,22 +6,31 @@ import io.github.regularcommands.converter.Parameter;
 import io.github.regularcommands.util.Permissions;
 import io.github.regularcommands.validator.CommandValidator;
 import io.github.zap.zombies.Zombies;
+import io.github.zap.zombies.command.mapeditor.EditorContext;
 import io.github.zap.zombies.command.mapeditor.MapeditorValidators;
+import io.github.zap.zombies.game.data.map.MapData;
 import org.bukkit.entity.Player;
 
-public class NewSessionForm extends CommandForm {
+public class NewMapForm extends CommandForm {
     private static final Parameter[] parameters = new Parameter[] {
             new Parameter("map"),
-            new Parameter("session")
+            new Parameter("create"),
+            new Parameter("^([a-zA-Z0-9_ ]+)$", "[name]")
     };
 
-    public NewSessionForm() {
+    private static final CommandValidator validator;
+
+    static {
+        validator = MapeditorValidators.mapAbsentValidator(2, MapeditorValidators.NO_ACTIVE_MAP);
+    }
+
+    public NewMapForm() {
         super("Creates a new session with the mapeditor.", Permissions.OPERATOR, parameters);
     }
 
     @Override
     public CommandValidator getValidator(Context context, Object[] arguments) {
-        return MapeditorValidators.NO_EDITOR_CONTEXT;
+        return validator;
     }
 
     @Override
@@ -29,9 +38,11 @@ public class NewSessionForm extends CommandForm {
         Zombies zombies = Zombies.getInstance();
         Player player = (Player)context.getSender();
 
-        zombies.getContextManager().newContext(player);
-        player.getInventory().addItem(zombies.getContextManager().getEditorItem());
+        MapData map = new MapData((String)arguments[2], player.getWorld().getName());
+        EditorContext editorContext = zombies.getContextManager().fetchContext(player);
 
-        return "Started a new mapeditor session.";
+        editorContext.setEditingMap(map);
+
+        return String.format("Created new map '%s'.", map.getName());
     }
 }
