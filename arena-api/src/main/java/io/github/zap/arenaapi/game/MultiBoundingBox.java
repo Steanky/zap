249f788f0e879c7da65c90725a3842f84fa76f3f
John-DND <dnd.initiative.tracker@gmail.com>
2021-02-16 01:37:32 -0800

    More bug fixes and optimizations for contains() method.

--- arena-api/src/main/java/io/github/zap/arenaapi/game/MultiBoundingBox.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/game/MultiBoundingBox.java
@@ -35,60 +35,73 @@ public class MultiBoundingBox {
         return false;
     }
 
-    private boolean contains(BoundingBox test, int startingIndex) {
-        for(int i = startingIndex; i < boundingBoxes.size(); i++) {
-            BoundingBox sample = boundingBoxes.get(i);
+    private boolean contains(BoundingBox test, List<BoundingBox> sampleList) {
+        for(int i = sampleList.size() - 1; i >= 0; i--) {
+            BoundingBox sample = sampleList.get(i);
 
             if(sample.contains(test)) { //simplest case; our box definitely fits
                 return true;
             }
-            else {
+            else if(sample.overlaps(test)) { //we must check further
                 Vector diffs;
 
                 Vector sampleEnd;
                 Vector testStart;
                 Vector testEnd;
 
-                boolean max;
-                if(sample.contains(test.getMin())) { //test max is out of bounds
-                    diffs = test.getMax().subtract(sample.getMax());
-
-                    sampleEnd = sample.getMax();
-                    testStart = test.getMin();
-                    testEnd = test.getMax();
-
-                    max = true;
-                } else if(sample.contains(test.getMax())) { //test min is out of bounds
-                    diffs = sample.getMin().subtract(test.getMin());
-
-                    sampleEnd = sample.getMin(); //reverse everything so we can use the same variables
-                    testStart = test.getMax();
-                    testEnd = test.getMin();
-
-                    max = false;
-                }
-                else {
-                    continue;
-                }
-
                 boolean xIn;
                 boolean yIn;
                 boolean zIn;
 
-                if(max) {
+                if(sample.contains(test.getMin())) { //test max is out of bounds; determine how much
+                    sampleEnd = sample.getMax();
+                    testStart = test.getMin();
+                    testEnd = test.getMax();
+
+                    diffs = testEnd.clone().subtract(sampleEnd);
+
+                    /*
+                    to be considered 'inside' for a particular axis, diff must be < 0 since bounding boxes do not
+                    consider vertices that exactly == maxValue to be inside.
+                     */
                     xIn = diffs.getX() < 0;
                     yIn = diffs.getY() < 0;
                     zIn = diffs.getZ() < 0;
                 }
-                else {
+                else if(sample.contains(test.getMax())) { //test min is out of bounds
+                    sampleEnd = sample.getMin();
+                    testStart = test.getMax();
+                    testEnd = test.getMin();
+
+                    diffs = sampleEnd.clone().subtract(testEnd);
+
+                    //for min comparison, if diff == 0, we are inside — no need to test adjacent bounds
                     xIn = diffs.getX() <= 0;
                     yIn = diffs.getY() <= 0;
                     zIn = diffs.getZ() <= 0;
                 }
+                else { //sample contains neither corner, but may be needed in later check, so don't remove it
+                    continue;
+                }
 
-                return (xIn || contains(BoundingBox.of(new Vector(sampleEnd.getX(), testStart.getY(), testStart.getZ()), testEnd), i + 1)) &&
-                        (yIn || contains(BoundingBox.of(new Vector(testStart.getX(), sampleEnd.getY(), testStart.getZ()), new Vector(sampleEnd.getX(), testEnd.getY(), testEnd.getZ())), i + 1)) &&
-                        (zIn || contains(BoundingBox.of(new Vector(testStart.getX(), testStart.getY(), sampleEnd.getZ()), new Vector(sampleEnd.getX(), sampleEnd.getY(), testEnd.getZ())), i + 1));
+                //don't iterate the sample that triggered this recursive check anymore
+                sampleList.remove(i);
+
+                /*
+                very readable code below. creates and tests up to 3 new bounds that are non-overlapping slices of the
+                original test bounds. does not execute a recursive call for 'slices' that are entirely located within
+                the sample bounds, which is the purpose of xIn, yIn, and zIn + short circuit or.
+
+                furthermore, the x-facing bounding box is the largest, followed by the y-facing, followed by the
+                z-facing. the largest is tested first as it is the most likely to not fit, thus preventing additional
+                recursive calls.
+                 */
+                return (xIn || contains(BoundingBox.of(new Vector(sampleEnd.getX(), testStart.getY(), testStart.getZ()), testEnd), sampleList)) &&
+                        (yIn || contains(BoundingBox.of(new Vector(testStart.getX(), sampleEnd.getY(), testStart.getZ()), new Vector(sampleEnd.getX(), testEnd.getY(), testEnd.getZ())), sampleList)) &&
+                        (zIn || contains(BoundingBox.of(new Vector(testStart.getX(), testStart.getY(), sampleEnd.getZ()), new Vector(sampleEnd.getX(), sampleEnd.getY(), testEnd.getZ())), sampleList));
+            }
+            else { //we don't need to keep samples that don't at least overlap in the list; may speed up things a bit
+                sampleList.remove(i);
             }
         }
 
@@ -96,14 +109,13 @@ public class MultiBoundingBox {
     }
 
     /**
-     * Determines if the provided bounding box is entirely contained within this MultiBoundingBox. Uses a recursive
-     * algorithm to account for cases in which two (or more) bounding boxes are directly next to each other, with test
-     * situated such that it can be found in both bounds at once.
+     * Uses a recursive algorithm to determine if the given BoundingBox is contained within the bounds specified by this
+     * MultiBoundingBox.
      * @param test The BoundingBox to test
-     * @return Whether or not this BoundingBox is entirely contained within this MultiBoundingBox.
+     * @return Whether or not test is entirely contained within the MultiBoundingBox, with no overlapping portions.
      */
     public boolean contains(BoundingBox test) {
-        return contains(test, 0);
+        return contains(test, new ArrayList<>(boundingBoxes));
     }
 
     /**
