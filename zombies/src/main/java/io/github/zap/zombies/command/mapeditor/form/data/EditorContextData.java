29f0e98919b1f1e1bcae6576f2d8b0eb71e25bbc
John-DND <dnd.initiative.tracker@gmail.com>
2021-02-23 21:45:45 -0800

    More lombok.

--- zombies/src/main/java/io/github/zap/zombies/command/mapeditor/form/data/EditorContextData.java
+++ zombies/src/main/java/io/github/zap/zombies/command/mapeditor/form/data/EditorContextData.java
@@ -2,15 +2,12 @@ package io.github.zap.zombies.command.mapeditor.form.data;
 
 import io.github.zap.zombies.command.mapeditor.EditorContext;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 
 @Getter
+@RequiredArgsConstructor
 public class EditorContextData {
     private final Player player;
     private final EditorContext context;
-
-    public EditorContextData(Player player, EditorContext context) {
-        this.player = player;
-        this.context = context;
-    }
 }
