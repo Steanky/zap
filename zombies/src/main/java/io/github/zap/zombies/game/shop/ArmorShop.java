45cab03e5d9bbfa4425300256bc4b87479a1a1ab
John-DND <dnd.initiative.tracker@gmail.com>
2021-03-27 13:49:42 -0700

    Undo changes to ArmorShop and potentially fix healthscale issue

--- zombies/src/main/java/io/github/zap/zombies/game/shop/ArmorShop.java
+++ zombies/src/main/java/io/github/zap/zombies/game/shop/ArmorShop.java
@@ -63,6 +63,7 @@ public class ArmorShop extends ArmorStandShop<ArmorShopData> {
 
         ArmorStand armorStand = getArmorStand();
         Location armorStandLocation = getArmorStand().getLocation().clone();
+        armorStandLocation.add(0, 1.5, 0);
         armorStandLocation.setYaw(getShopData().getArmorStandDirection());
         armorStand.teleport(armorStandLocation);
         armorStand.setSmall(true);
