f716fb9745bf1c74a26e4308e2504ff954be4ce0
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2021-03-29 13:12:42 -0400

    fix up gradle oversights

--- arena-api/build.gradle.kts
+++ arena-api/build.gradle.kts
@@ -10,26 +10,15 @@ java {
     }
 }
 
-val bukkitPlugin: Configuration by configurations.creating {
-    isTransitive = false
-}
 val shade: Configuration by configurations.creating {
     isTransitive = false
 }
-configurations.api.get().extendsFrom(bukkitPlugin, shade)
+val bukkitPlugin: Configuration by configurations.creating {
+    isTransitive = false
+}
+configurations.api.get().extendsFrom(shade, bukkitPlugin)
 
 val pluginDir = "${System.getProperty("outputDir") ?: "../run/server-1"}/plugins"
-
-repositories {
-    mavenCentral()
-    mavenLocal()
-    maven {
-        url = uri("https://jitpack.io")
-    }
-    maven {
-        url = uri("https://maven.springframework.org/release")
-    }
-}
 
 dependencies {
     api("com.destroystokyo.paper:paper:1.16.5-R0.1-SNAPSHOT")
@@ -44,22 +33,23 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.4")
 }
 
-tasks.withType<Jar> {
-    destinationDirectory.set(File(pluginDir))
-    from (shade.map {
-        if (it.isDirectory) it else zipTree(it)
-    })
-}
-
 tasks.register<Copy>("copyPlugins") {
     from(bukkitPlugin).into(pluginDir)
-    bukkitPlugin.allDependencies.forEach {
-        rename("-${it.version}", "")
-    }
 }
 
 tasks.compileJava {
     dependsOn("copyPlugins")
+}
+
+tasks.processResources {
+    expand("version" to version)
+}
+
+tasks.jar {
+    destinationDirectory.set(File(pluginDir))
+    from (shade.map {
+        if (it.isDirectory) it else zipTree(it)
+    })
 }
 
 description = "arena-api"
