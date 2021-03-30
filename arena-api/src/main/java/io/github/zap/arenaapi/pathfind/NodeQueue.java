79830ff3b155ba07bbd40392217565806a5fd0c9
John-DND <dnd.initiative.tracker@gmail.com>
2021-03-30 13:38:48 -0700

    copy pasting the java impl with some adjustments lol

--- arena-api/src/main/java/io/github/zap/arenaapi/pathfind/NodeQueue.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/pathfind/NodeQueue.java
@@ -5,26 +5,19 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class NodeQueue {
+    private static final Comparator<PathNode> COMPARATOR = NodeComparator.instance();
     private static final int DEFAULT_INITIAL_CAPACITY = 11;
-    transient PathNode[] queue;
-    int size;
-    private final Comparator<PathNode> comparator = NodeComparator.instance();
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+    private PathNode[] queue;
+    private int size;
 
     public NodeQueue() {
         this(DEFAULT_INITIAL_CAPACITY);
     }
 
     public NodeQueue(int initialCapacity) {
-        if (initialCapacity < 1)
-            throw new IllegalArgumentException();
         this.queue = new PathNode[initialCapacity];
     }
-
-    private static Object[] ensureNonEmpty(Object[] es) {
-        return (es.length > 0) ? es : new Object[1];
-    }
-
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
 
     private void grow(int minCapacity) {
         int oldCapacity = queue.length;
@@ -37,7 +30,7 @@ class NodeQueue {
     }
 
     private static int hugeCapacity(int minCapacity) {
-        if (minCapacity < 0) // overflow
+        if (minCapacity < 0)
             throw new OutOfMemoryError();
         return (minCapacity > MAX_ARRAY_SIZE) ?
                 Integer.MAX_VALUE :
@@ -57,6 +50,17 @@ class NodeQueue {
         siftUp(i, e);
         size = i + 1;
         return true;
+    }
+
+    public void update(PathNode original, PathNode newValue) {
+        int comparisonResult = COMPARATOR.compare(original, newValue);
+
+        if(comparisonResult < 0) {
+            siftUp(indexOf(original), newValue);
+        }
+        else if(comparisonResult > 0) {
+            siftDown(indexOf(original), newValue);
+        }
     }
 
     public PathNode peek() {
@@ -161,16 +165,15 @@ class NodeQueue {
             final PathNode x = queue[(n = --size)];
             queue[n] = null;
             if (n > 0) {
-                siftDownUsingComparator(0, x, queue, n, comparator);
+                siftDown(0, x);
             }
         }
         return result;
     }
 
     PathNode removeAt(int i) {
-        // assert i >= 0 && i < size;
         int s = --size;
-        if (s == i) // removed last element
+        if (s == i)
             queue[i] = null;
         else {
             PathNode moved = queue[s];
@@ -186,62 +189,52 @@ class NodeQueue {
     }
 
     private void siftUp(int k, PathNode x) {
-            siftUpUsingComparator(k, x, queue, comparator);
-    }
-
-    private static void siftUpUsingComparator(int k, PathNode x, PathNode[] es, Comparator<PathNode> cmp) {
         while (k > 0) {
             int parent = (k - 1) >>> 1;
-            PathNode e = es[parent];
-            if (cmp.compare(x, e) >= 0)
+            PathNode e = queue[parent];
+            if (COMPARATOR.compare(x, e) >= 0)
                 break;
-            es[k] = e;
+            queue[k] = e;
             k = parent;
         }
-        es[k] = x;
+        queue[k] = x;
     }
 
     private void siftDown(int k, PathNode x) {
-        siftDownUsingComparator(k, x, queue, size, comparator);
-    }
-
-    private static void siftDownUsingComparator(int k, PathNode x, PathNode[] es, int n, Comparator<PathNode> cmp) {
-        // assert n > 0;
-        int half = n >>> 1;
+        int half = size >>> 1;
         while (k < half) {
             int child = (k << 1) + 1;
-            PathNode c = es[child];
+            PathNode c = queue[child];
             int right = child + 1;
-            if (right < n && cmp.compare(c, es[right]) > 0)
-                c = es[child = right];
-            if (cmp.compare(x, c) <= 0)
+            if (right < size && COMPARATOR.compare(c, queue[right]) > 0)
+                c = queue[child = right];
+            if (COMPARATOR.compare(x, c) <= 0)
                 break;
-            es[k] = c;
+            queue[k] = c;
             k = child;
         }
-        es[k] = x;
+        queue[k] = x;
     }
 
     private void heapify() {
         int n = size, i = (n >>> 1) - 1;
         for (; i >= 0; i--)
-            siftDownUsingComparator(i, queue[i], queue, n, comparator);
-
+            siftDown(i, queue[i]);
     }
 
     public Comparator<PathNode> comparator() {
-        return comparator;
+        return COMPARATOR;
     }
 
     public final Spliterator<PathNode> spliterator() {
-        return new NodeQueue.PriorityQueueSpliterator(0, -1);
+        return new PathNodeSpliterator(0, -1);
     }
 
-    final class PriorityQueueSpliterator implements Spliterator<PathNode> {
+    private final class PathNodeSpliterator implements Spliterator<PathNode> {
         private int index;
         private int fence;
 
-        PriorityQueueSpliterator(int origin, int fence) {
+        PathNodeSpliterator(int origin, int fence) {
             this.index = origin;
             this.fence = fence;
         }
@@ -254,9 +247,9 @@ class NodeQueue {
             return hi;
         }
 
-        public NodeQueue.PriorityQueueSpliterator trySplit() {
+        public PathNodeSpliterator trySplit() {
             int hi = getFence(), lo = index, mid = (lo + hi) >>> 1;
-            return (lo >= mid) ? null : new NodeQueue.PriorityQueueSpliterator(lo, index = mid);
+            return (lo >= mid) ? null : new PathNodeSpliterator(lo, index = mid);
         }
 
         public void forEachRemaining(Consumer<? super PathNode> action) {
@@ -318,18 +311,16 @@ class NodeQueue {
         return (bits[i >> 6] & (1L << i)) == 0;
     }
 
-    /** Implementation of bulk remove methods. */
     private boolean bulkRemove(Predicate<? super PathNode> filter) {
         final int end = size;
         int i;
-        for (i = 0; i < end && !filter.test(queue[i]); i++)
-            ;
+        for (i = 0; i < end && !filter.test(queue[i]); i++);
         if (i >= end) {
             return false;
         }
         final int beg = i;
         final long[] deathRow = nBits(end - beg);
-        deathRow[0] = 1L;   // set bit 0
+        deathRow[0] = 1L;
         for (i = beg + 1; i < end; i++)
             if (filter.test(queue[i]))
                 setBit(deathRow, i - beg);
@@ -343,9 +334,6 @@ class NodeQueue {
         return true;
     }
 
-    /**
-     * @throws NullPointerException {@inheritDoc}
-     */
     public void forEach(Consumer<? super PathNode> action) {
         Objects.requireNonNull(action);
         for (int i = 0, n = size; i < n; i++)
--- arena-api/src/main/java/io/github/zap/arenaapi/pathfind/Test.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/pathfind/Test.java
@@ -12,10 +12,11 @@ public class Test {
         for(int i = 0; i < 10; i++) {
             PathNode node = new PathNode(i + random.nextInt(2 * (i + 1)), i + random.nextInt(2 * (i + 1)), i + random.nextInt(2 * (i + 1)));
             node.score = new Score(i + random.nextInt(2 * (i + 1)), i + random.nextInt(2 * (i + 1)));
+            queue.add(node);
         }
 
         while(queue.size() > 0) {
-
+            ArenaApi.info(queue.poll().toString());
         }
 
         ArenaApi.info("t");
