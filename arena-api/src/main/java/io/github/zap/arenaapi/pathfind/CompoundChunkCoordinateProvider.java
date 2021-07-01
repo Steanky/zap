61f2a7919163d6b154f227e254e3827d7b077955
John-DND <dnd.initiative.tracker@gmail.com>
2021-07-01 03:43:47 -0700

    Simplified CompoundChunkCoordinateProvider

--- arena-api/src/main/java/io/github/zap/arenaapi/pathfind/CompoundChunkCoordinateProvider.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/pathfind/CompoundChunkCoordinateProvider.java
@@ -2,58 +2,25 @@ package io.github.zap.arenaapi.pathfind;
 
 import io.github.zap.vector.ChunkVectorAccess;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
 public class CompoundChunkCoordinateProvider implements ChunkCoordinateProvider {
-    private static class ChunkVectorEntry {
-        private final ChunkVectorAccess vectorAccess;
-        private final ChunkCoordinateProvider linkedProvider;
-
-        private ChunkVectorEntry(@NotNull ChunkVectorAccess vectorAccess,
-                                 @Nullable ChunkCoordinateProvider linkedProvider) {
-            this.vectorAccess = vectorAccess;
-            this.linkedProvider = linkedProvider;
-        }
-
-        @Override
-        public int hashCode() {
-            return vectorAccess.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if(obj instanceof ChunkVectorEntry) {
-                ChunkVectorEntry other = (ChunkVectorEntry)obj;
-                return other.vectorAccess.equals(this.vectorAccess);
-            }
-
-            return false;
-        }
-    }
-
-    private final Map<ChunkVectorEntry, ChunkVectorEntry> mergedCoordinates;
+    private final Set<ChunkVectorAccess> mergedCoordinates;
 
     public CompoundChunkCoordinateProvider() {
-        mergedCoordinates = new HashMap<>();
+        mergedCoordinates = new HashSet<>();
     }
 
     public void union(@NotNull ChunkCoordinateProvider provider) {
         for(ChunkVectorAccess vectorAccess : provider) {
-            ChunkVectorEntry entry = new ChunkVectorEntry(vectorAccess, provider);
-            mergedCoordinates.put(entry, entry);
+            mergedCoordinates.add(vectorAccess);
         }
     }
 
     @Override
     public boolean hasChunk(int x, int z) {
-        ChunkVectorEntry entry = mergedCoordinates.get(new ChunkVectorEntry(ChunkVectorAccess.immutable(x, z), null));
-        if(entry != null && entry.linkedProvider != null) {
-            return entry.linkedProvider.hasChunk(x, z);
-        }
-
-        return false;
+        return mergedCoordinates.contains(ChunkVectorAccess.immutable(x, z));
     }
 
     @Override
@@ -64,6 +31,6 @@ public class CompoundChunkCoordinateProvider implements ChunkCoordinateProvider 
     @NotNull
     @Override
     public Iterator<ChunkVectorAccess> iterator() {
-        return mergedCoordinates.values().stream().map(t -> t.vectorAccess).iterator();
+        return mergedCoordinates.iterator();
     }
 }
