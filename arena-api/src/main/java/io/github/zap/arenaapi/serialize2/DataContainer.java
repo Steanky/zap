c73ce8128bbf899a4e52de5eb755ba3debc18289
John-DND <dnd.initiative.tracker@gmail.com>
2021-07-26 04:02:46 -0700

    standard DataContainer

--- arena-api/src/main/java/io/github/zap/arenaapi/serialize2/DataContainer.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/serialize2/DataContainer.java
@@ -8,7 +8,23 @@ import java.util.Optional;
  * An object containing a number of key-value pairs.
  */
 public interface DataContainer {
-    <T> @NotNull Optional<T> getObject(@NotNull DataKey key);
+    <T> @NotNull Optional<T> getObject(@NotNull DataKey key, @NotNull Class<T> type);
 
-    <T> T getObjectOrDefault(@NotNull DataKey key, T fallback);
+    @NotNull Optional<String> getString(@NotNull DataKey key);
+
+    @NotNull Optional<Boolean> getBoolean(@NotNull DataKey key);
+
+    @NotNull Optional<Byte> getByte(@NotNull DataKey key);
+
+    @NotNull Optional<Short> getShort(@NotNull DataKey key);
+
+    @NotNull Optional<Integer> getInt(@NotNull DataKey key);
+
+    @NotNull Optional<Character> getChar(@NotNull DataKey key);
+
+    @NotNull Optional<Long> getLong(@NotNull DataKey key);
+
+    @NotNull Optional<Float> getFloat(@NotNull DataKey key);
+
+    @NotNull Optional<Double> getDouble(@NotNull DataKey key);
 }
--- arena-api/src/main/java/io/github/zap/arenaapi/serialize2/StandardDataContainer.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/serialize2/StandardDataContainer.java
@@ -14,15 +14,61 @@ class StandardDataContainer implements DataContainer {
     }
 
     @Override
-    public @NotNull <T> Optional<T> getObject(@NotNull DataKey key) {
-        //noinspection unchecked
-        return Optional.ofNullable((T)map.get(key.key()));
+    public @NotNull <T> Optional<T> getObject(@NotNull DataKey key, @NotNull Class<T> type) {
+        Object data = map.get(key.key());
+        if(data == null) {
+            return Optional.empty();
+        }
+        else if(type.isAssignableFrom(data.getClass())) {
+            //noinspection unchecked
+            return Optional.of((T)data);
+        }
+
+        return Optional.empty();
     }
 
     @Override
-    public <T> T getObjectOrDefault(@NotNull DataKey key, T fallback) {
-        //noinspection unchecked
-        T stored = (T)map.get(key.key());
-        return stored == null ? fallback : stored;
+    public @NotNull Optional<String> getString(@NotNull DataKey key) {
+        return getObject(key, String.class);
+    }
+
+    @Override
+    public @NotNull Optional<Boolean> getBoolean(@NotNull DataKey key) {
+        return getObject(key, Boolean.class);
+    }
+
+    @Override
+    public @NotNull Optional<Byte> getByte(@NotNull DataKey key) {
+        return getObject(key, Byte.class);
+    }
+
+    @Override
+    public @NotNull Optional<Short> getShort(@NotNull DataKey key) {
+        return getObject(key, Short.class);
+    }
+
+    @Override
+    public @NotNull Optional<Integer> getInt(@NotNull DataKey key) {
+        return getObject(key, Integer.class);
+    }
+
+    @Override
+    public @NotNull Optional<Character> getChar(@NotNull DataKey key) {
+        return getObject(key, Character.class);
+    }
+
+    @Override
+    public @NotNull Optional<Long> getLong(@NotNull DataKey key) {
+        return getObject(key, Long.class);
+    }
+
+    @Override
+    public @NotNull Optional<Float> getFloat(@NotNull DataKey key) {
+        return getObject(key, Float.class);
+    }
+
+    @Override
+    public @NotNull Optional<Double> getDouble(@NotNull DataKey key) {
+        return getObject(key, Double.class);
     }
 }
