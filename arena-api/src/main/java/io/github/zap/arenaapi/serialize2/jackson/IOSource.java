038e4b924de50661d3f068ad49d0b5caae984907
John-DND <dnd.initiative.tracker@gmail.com>
2021-08-05 21:34:50 -0700

    added util methods to IOSource

--- arena-api/src/main/java/io/github/zap/arenaapi/serialize2/jackson/IOSource.java
+++ arena-api/src/main/java/io/github/zap/arenaapi/serialize2/jackson/IOSource.java
@@ -2,11 +2,73 @@ package io.github.zap.arenaapi.serialize2.jackson;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Supplier;
 
+/**
+ * Represents a simple source of input and output.
+ */
 public interface IOSource {
-    @NotNull InputStream newInput();
+    /**
+     * Creates a new InputStream from this IOSource.
+     *
+     * Since the stream may be holding on to some resource, it is important to call close() on it after it has been
+     * used.
+     * @return A new InputStream for this IOSource
+     * @throws IOException When the underlying stream creation throws an IOException
+     */
+    @NotNull InputStream newInput() throws IOException;
 
-    @NotNull OutputStream newOutput();
+    /**
+     * Creates a new OutputStream from this IOSource.
+     *
+     * Since the stream may be holding on to some resource, it is important to call close() on it after it has been
+     * used.
+     * @return A new OutputStream for this IOSource
+     * @throws IOException When the underlying stream creation throws an IOException
+     */
+    @NotNull OutputStream newOutput() throws IOException;
+
+    static @NotNull IOSource fromFile(@NotNull File file) {
+        return new IOSource() {
+            @Override
+            public @NotNull InputStream newInput() throws FileNotFoundException {
+                return new FileInputStream(file);
+            }
+
+            @Override
+            public @NotNull OutputStream newOutput() throws FileNotFoundException {
+                return new FileOutputStream(file);
+            }
+        };
+    }
+
+    static @NotNull IOSource fromString(@NotNull String string) {
+        return new IOSource() {
+            @Override
+            public @NotNull InputStream newInput() {
+                return new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+            }
+
+            @Override
+            public @NotNull OutputStream newOutput() {
+                return new ByteArrayOutputStream(0);
+            }
+        };
+    }
+
+    static @NotNull IOSource fromStreams(@NotNull Supplier<InputStream> inputStream, @NotNull Supplier<OutputStream> outputStream) {
+        return new IOSource() {
+            @Override
+            public @NotNull InputStream newInput() {
+                return inputStream.get();
+            }
+
+            @Override
+            public @NotNull OutputStream newOutput() {
+                return outputStream.get();
+            }
+        };
+    }
 }
